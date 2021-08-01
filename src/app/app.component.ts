480416dbe81d356ef5923d5a4726566b9f86f19d
Alan <alannmv@hotmail.com>
2021-08-01 17:10:53 -0600

    interceptors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuariosService } from './services/usuarios.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interceptorApp';
+
+  constructor( private usuariosService: UsuariosService ) {
+
+    this.usuariosService.obtenerUsuarios()
+      .subscribe( resp => {
+        console.log(resp);
+      }, (err) => {
+        // Aquí se puede hacer cambios en el html
+        // mostrar una pantalla o una alerta
+        console.log('Error en el appComponent');
+      });
+
+  }  
+
 }
---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor( private http: HttpClient ) { }
+
+  obtenerUsuarios() {
+
+    let params = new HttpParams().append('page', '1');
+    params = params.append('nombre', 'Alan Miranda');
+
+    const headers = new HttpHeaders({
+      'token-usuario': 'ABC548746ASD684EW24AS3D21'
+    });
+
+    return this.http.get('https://reqres23432.in/api/user', {
+      params,
+      headers
+    }).pipe(
+      map( resp => resp['data'] ),
+      catchError( this.manejarError )
+      // catchError( err => {
+
+      //   console.log('Sucedió un error');
+      //   console.log('Registrado en el log file');
+      //   console.warn(err);
+
+      //   // catchError necesita retornar un observable
+      //   return throwError('Error personalizado');
+
+      // })
+    )
+
+  }
+
+  manejarError( error: HttpErrorResponse ) {
+    console.log('Sucedió un error');
+    console.log('Registrado en el log file');
+    console.warn(error);
+
+    // catchError necesita retornar un observable
+    return throwError('Error personalizado');
+  }
+  
+}
